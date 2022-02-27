cc1618d7ff4cbce1b08b47f541f59fa2ae69d40c
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2022-02-27 09:44:23 +0530

    Initial Files

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["ssl.gstatic.com", "images.unsplash.com", "lh3.googleusercontent.com", "i.stack.imgur.com"]
+  }
+}
